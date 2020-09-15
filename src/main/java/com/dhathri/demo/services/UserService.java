7f965e94d304d10382d0912791a07a1ce17ff2c5
dhathri <dhathrivupparapalli@gmail.com>
2020-09-15 13:23:26 +0530

    updated

--- src/main/java/com/dhathri/demo/services/UserService.java
+++ src/main/java/com/dhathri/demo/services/UserService.java
@@ -26,6 +26,12 @@ public class UserService {
 		int len=allUser.size();
 		int i=0;
 		User newUser=null;
+		if(len==0) {
+			int bal=200000;
+			user.balance=bal-user.transactionamount;
+			userRepository.save(user);
+		}
+		else {
 		for(User res:allUser)
 		{
 			if(i == len-1)
@@ -35,12 +41,14 @@ public class UserService {
 			}
 			i++;
 		}	
+		
+		
 		int bal=newUser.balance;
 		System.out.println(bal);
 		user.balance=bal-user.transactionamount;
 		userRepository.save(user);
 	}
-
+	}
 	public List<User> showAllTransactions(){ 
 		 
 		List<User> re=userRepository.findAll();
@@ -49,9 +57,14 @@ public class UserService {
 
 	public List<User> showTop5Transactions(){        
 		 List<User> re=userRepository.findAll();
-		 Collections.reverse(re); 
+		 Collections.reverse(re);
+		 int leng=re.size();
+		 if(leng<=5) {
+			 return re;
+		 }
+		 else {
 		 List<User> arrlist2 = re.subList(0, 5);
 		 return arrlist2;
 	 }
-
+	}
 }
